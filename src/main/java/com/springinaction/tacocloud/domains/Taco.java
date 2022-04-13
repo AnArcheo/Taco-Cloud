62b1dd1070f5446be12c6c60a22b49da73ad732c
Anna Łuczak <an.archeo@gmail.com>
2022-04-13 21:31:57 +0100

    created Taco domain

---
+++ src/main/java/com/springinaction/tacocloud/domains/Taco.java
@@ -0,0 +1,12 @@
+package com.springinaction.tacocloud.domains;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
