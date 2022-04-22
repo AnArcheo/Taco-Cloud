f6a6960be907ddd1b1e7858c509a9b8622bf7c71
Anna Łuczak <an.archeo@gmail.com>
2022-04-22 23:08:28 +0100

    added validation check to OrderController

--- src/main/java/com/springinaction/tacocloud/controllers/OrderController.java
+++ src/main/java/com/springinaction/tacocloud/controllers/OrderController.java
@@ -3,11 +3,14 @@ package com.springinaction.tacocloud.controllers;
 import com.springinaction.tacocloud.domains.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -21,7 +24,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus){
+    public String processOrder(@Valid TacoOrder tacoOrder, Errors errors, SessionStatus sessionStatus){
+
+        if(errors.hasErrors()){
+            return "orderForm";
+        }
         log.info("Order Submitted: {}", tacoOrder);
         sessionStatus.setComplete();
         return "redirect:/";
