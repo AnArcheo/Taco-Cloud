ebd388f7198c2085ef15de86bee61fc36c7727c1
Anna Łuczak <an.archeo@gmail.com>
2022-06-11 23:31:44 +0100

    updated OrderRepository - added CrudRepository

--- src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
+++ src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.springinaction.tacocloud.repositories;
 
 import com.springinaction.tacocloud.domains.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     TacoOrder save(TacoOrder order);
 }
