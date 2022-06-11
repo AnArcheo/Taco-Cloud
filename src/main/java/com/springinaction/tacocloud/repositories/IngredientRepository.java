8771c401ddc33038addecc9843b809ee9e75a371
Anna Łuczak <an.archeo@gmail.com>
2022-06-11 23:32:01 +0100

    updated IngredientRepository - added CrudRepository

--- src/main/java/com/springinaction/tacocloud/repositories/IngredientRepository.java
+++ src/main/java/com/springinaction/tacocloud/repositories/IngredientRepository.java
@@ -1,10 +1,12 @@
 package com.springinaction.tacocloud.repositories;
 
 import com.springinaction.tacocloud.domains.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
     Optional<Ingredient> findById(String id);
