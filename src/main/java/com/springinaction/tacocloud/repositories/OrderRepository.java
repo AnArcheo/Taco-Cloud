e9c8a29f980ff692c27efe4d2d5eb208f0c42fc2
Anna Łuczak <an.archeo@gmail.com>
2022-06-12 21:42:43 +0100

    added new method to OrderRepository

--- src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
+++ src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
@@ -3,9 +3,12 @@ package com.springinaction.tacocloud.repositories;
 import com.springinaction.tacocloud.domains.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 // Not needed - extends CrudRepository
 //    TacoOrder save(TacoOrder order);
+    //Additional method to basic CRUD methods
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
 }
