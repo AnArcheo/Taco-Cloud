856d7b9230195348a25896604a3517c57c84d987
Anna Łuczak <an.archeo@gmail.com>
2022-06-16 22:25:11 +0100

    updated TacoCloudApplication

--- src/main/java/com/springinaction/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/springinaction/tacocloud/TacoCloudApplication.java
@@ -1,18 +1,8 @@
 package com.springinaction.tacocloud;
 
-import com.springinaction.tacocloud.services.UserDetailsService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class TacoCloudApplication {
