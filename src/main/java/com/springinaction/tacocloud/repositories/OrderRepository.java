66b9493355d044da86762aa6ce3fb63e1ff05c8e
Anna Łuczak <an.archeo@gmail.com>
2022-06-19 22:31:18 +0100

    updated OrderRepository

--- src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
+++ src/main/java/com/springinaction/tacocloud/repositories/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.springinaction.tacocloud.repositories;
 
 import com.springinaction.tacocloud.domains.TacoOrder;
+import com.springinaction.tacocloud.domains.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,5 +12,6 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 // Not needed - extends CrudRepository
 //    TacoOrder save(TacoOrder order);
     //Additional method to basic CRUD methods
-    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    //List<TacoOrder> findByDeliveryZip(String deliveryZip);
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
