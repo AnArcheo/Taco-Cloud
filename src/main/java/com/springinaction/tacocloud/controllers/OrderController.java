50c5336e6501490cf64c166ae78cb61c61c1f16a
Anna Łuczak <an.archeo@gmail.com>
2022-06-19 22:31:37 +0100

    updated OrderController

--- src/main/java/com/springinaction/tacocloud/controllers/OrderController.java
+++ src/main/java/com/springinaction/tacocloud/controllers/OrderController.java
@@ -4,8 +4,13 @@ import com.springinaction.tacocloud.domains.TacoOrder;
 import com.springinaction.tacocloud.domains.User;
 import com.springinaction.tacocloud.repositories.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +26,24 @@ import javax.validation.Valid;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private int pageSize = 20; //limit the number of orders displayed to most recent 20 orders
     private OrderRepository orderRepository;
 
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
 
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String ordersForUser(@AuthenticationPrincipal User user,
+                                Model model){
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
+    }
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
